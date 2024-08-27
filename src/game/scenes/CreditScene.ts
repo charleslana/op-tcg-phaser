546a2e1ebf4c16a2c6c5f85c6154ef7a03706a60
Charles <charleslanop@gmail.com>
2024-08-27 13:18:59 -0300

    updated

--- src/game/scenes/CreditScene.ts
+++ src/game/scenes/CreditScene.ts
@@ -16,16 +16,63 @@ export class CreditScene extends Scene {
   }
 
   create() {
-    this.createText();
+    this.createByText();
+    this.createDiscordButton();
+    this.createCreditsText();
+    this.createCreditListText();
     this.createBackButton();
     new Version(this);
     EventBus.emit('current-scene-ready', this);
   }
 
-  private createText(): void {
+  private createByText(): void {
     this.add
-      .text(0, 0, 'Créditos', {
-        fontSize: '45px',
+      .text(200, 100, 'Desenvolvido por\nCharles', {
+        fontSize: '25px',
+        color: '#000000',
+        fontFamily: 'AlineaSans',
+        align: 'center',
+      })
+      .setOrigin(0);
+  }
+
+  private createDiscordButton(): void {
+    const button = new ButtonBeige(this);
+    const buttonCreate = button.create({
+      positionX: 300,
+      positionY: 250,
+      text: 'Discord',
+      scaleX: 1.3,
+      scaleY: 2,
+    });
+    buttonCreate.on('pointerdown', () => window.open('https://discord.gg/seulink', '_blank'));
+  }
+
+  private createCreditsText(): void {
+    const { width } = this.scale;
+    this.add
+      .text(width - width / 3, 100, 'Agradecimentos e créditos:', {
+        fontSize: '25px',
+        color: '#000000',
+        fontFamily: 'AlineaSans',
+      })
+      .setOrigin(0);
+  }
+
+  private createCreditListText(): void {
+    const { width } = this.scale;
+    const lines = [
+      'One Piece',
+      'Inspirado em One Piece TCG Online Simulator por Batsu',
+      'Thierry Puyfoulhoux (Alinea Sans W01 Regular font)',
+      'Phaser3',
+      'rexUI Plugins',
+      'phaser-font-plugin',
+    ];
+    const text = lines.join('\n');
+    this.add
+      .text(width - width / 3, 170, text, {
+        fontSize: '20px',
         color: '#000000',
         fontFamily: 'AlineaSans',
       })
